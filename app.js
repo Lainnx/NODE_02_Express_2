f3e95e37f2dcf96f91324c14aa6b0fcd2850b681
das <tree@gmail.com>
2025-06-17 18:29:31 +0200

    2025/06/17

---
+++ app.js
@@ -0,0 +1,10 @@
+const path = require("node:path");
+const express = require("express");
+const app = express()
+
+process.loadEnvFile();
+const PORT = process.env.PORT || 8888
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(PORT, () => console.log(`Example app listening on port http://localhost:${PORT}!`))
